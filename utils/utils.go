370df2b80a2e9937404aa8e21b70bcb44be9c374
Леонова Елизавета Геннадьевна <e.g.leonova@tbank.ru>
2024-08-24 20:56:11 +0300

    Change func int

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(s []string, e string) bool {
+func InSlice(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
 			return true
@@ -9,7 +9,7 @@ func Contains(s []string, e string) bool {
 	return false
 }
 
-func ContainsInt(a []int, x int) bool {
+func InSliceInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
