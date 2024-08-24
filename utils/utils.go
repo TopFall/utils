b343581ae13881eb8812f8a32f12602f49d5077f
Леонова Елизавета Геннадьевна <e.g.leonova@tbank.ru>
2024-08-24 20:48:54 +0300

    add func for int

--- utils/utils.go
+++ utils/utils.go
@@ -8,3 +8,12 @@ func Contains(s []string, e string) bool {
 	}
 	return false
 }
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
